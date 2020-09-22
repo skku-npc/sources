8636962a60f5f32a3fdb5bcb7c594cb60f4acf73
leesj <31756944+leesj10147@users.noreply.github.com>
2020-09-22 20:40:01 +0900

    Create kmp.cpp
    close #11

---
+++ kmp.cpp
@@ -0,0 +1,26 @@
+void get_fail(const char* p, int len, vector<int>& fail)//xxxa..xxx f[end of x] => a, //0based
+{
+    fail.resize(len);
+    fail[0] = 0;
+    for (int i = 1, j = 0; i < len; ++i)
+    {
+        while(j && p[i] != p[j]) j = fail[j-1];
+        if (p[i] == p[j]) fail[i] = ++j;
+    }
+}
+void KMP(const char* text, int tlen, const char* pattern, int plen, vector<int>& fail, vector<int>& ans) //0based
+{
+    ans.clear();
+    for (int i = 0, j = 0; i < tlen; ++i)
+    {
+        while(j && text[i] != pattern[j]) j = fail[j-1];
+        if (text[i] == pattern[j])
+        {
+            if (j == plen - 1)
+            {
+                ans.push_back(i - j);
+                j = fail[j];
+            } else ++j;
+        }
+    }
+}
