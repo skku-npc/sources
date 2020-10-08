7d4f6c25670a6fc0e300a4952be8944441a13a0d
Jaemin Choi <1dotolee@gmail.com>
2020-10-08 22:20:03 +0900

    Reformat Kosaraju

--- kosaraju.cpp
+++ kosaraju.cpp
@@ -1,81 +1,35 @@
-#include <bits/stdc++.h>
-#define endl '\n'
-using namespace std;
-
-typedef long long ll;
-typedef pair<int, int> pii;
-
-vector<vector<int>> edge, inverse_edge;
-int v, e;
-
-void dfs(vector<bool> &check, stack<int> &visit, int curr)
-{
+int n;
+stack<int> &st;
+vector<bool> check;
+vector<vector<int>> adj, adj_r;
+void dfs(int curr) {
     check[curr] = true;
-    for (int next : edge[curr])
-    {
-        if (check[next])
-            continue;
-        dfs(check, visit, next);
-    }
-    visit.push(curr);
+    for (auto& next : adj[curr])
+        if (!check[next]) dfs(next);
+    st.push(curr);
 }
-
-set<set<int>> kosaraju(void)
-{
-    vector<bool> check(v + 1, false);
-    stack<int> visit;
-    for (int i = 1; i <= v; i++)
-    {
-        if (check[i])
-            continue;
-        dfs(check, visit, i);
-    }
-
-    check = vector<bool>(v + 1, false);
+set<set<int>> kosaraju() {
+    check.resize(n);
+    for (int i = 0; i < n; i++)
+        if (!check[i]) dfs(i);
+    check = vector<bool>(n + 1);
     set<set<int>> ret;
-    while (!visit.empty())
-    {
-        int node = visit.top();
-        visit.pop();
-        if (check[node])
-            continue;
-
+    while (!st.empty()) {
+        int node = st.top();
+        st.pop();
+        if (check[node]) continue;
+        set<int> se;
         stack<int> sk;
         sk.push(node);
-        set<int> se;
-        while (!sk.empty())
-        {
+        while (!sk.empty()) {
             int curr = sk.top();
             sk.pop();
             check[curr] = true;
             se.insert(curr);
-            for (int next : inverse_edge[curr])
-            {
-                if (check[next])
-                    continue;
-                sk.push(next);
-            }
+            for (auto next : adj_r[curr])
+                if (!check[next]) sk.push(next);
         }
         ret.insert(se);
     }
     return ret;
-}
-int main(void)
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    cin >> v >> e;
-    edge.resize(v + 1);
-    inverse_edge.resize(v + 1);
-    for (int i = 0; i < e; i++)
-    {
-        int a, b;
-        cin >> a >> b;
-        edge[a].push_back(b);
-        inverse_edge[b].push_back(a);
-    }
-    set<set<int>> scc = kosaraju();
-
-    return 0;
 }
