766f9c59c886c72e81e5dd498aa372c20273cca8
Jaemin Choi <1dotolee@gmail.com>
2020-10-08 21:50:39 +0900

    Reformat math.cpp

--- math.cpp
+++ math.cpp
@@ -1,18 +1,13 @@
 int MOD = 998244353;
-int power(int a, int n){
+int power(int a, int n) {
     int res = 1;
-    int now = a;
-    while(n) {
-        if (n&1) res  = (res*now) %MOD;
-        n>>=1;
-        now = (now*now)%MOD;
-    }
+    for (;n; n >>= 1, a = (a * a) % MOD)
+        if (n & 1) res = (res * a) % MOD;
     return res;
 }
-int inv(int a) {
-    return power(a, MOD-2);
-}
+int inv(int a) { return power(a, MOD - 2); }
 int C(int n, int r) {
-    if (n < r) return 0;
-    return fact[n] %MOD * inv(fact[n-r])%MOD * inv(fact[r])%MOD;
+    if (n < r)
+        return 0;
+    return fact[n] % MOD * inv(fact[n - r]) % MOD * inv(fact[r]) % MOD;
 }
