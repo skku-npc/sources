d205509bcd8318951200f84b87640d6ca970619a
Jaemin Choi <1dotolee@gmail.com>
2020-10-08 19:30:37 +0900

    Reformat Euler Phi

--- euler_phi.cpp
+++ euler_phi.cpp
@@ -1,14 +1,12 @@
-int phi(int n)
-{
+int phi(int n) {
     int ret = n;
-    for (int i=2;i*i<=n;++i)
-    {
-        if (n%i==0)
-        {
-            while(n%i==0) n/=i;
-            ret -= ret/i;
+    for (int i = 2; i * i <= n; ++i) {
+        if (n % i == 0) {
+            while (n % i == 0)
+                n /= i;
+            ret -= ret / i;
         }
     }
-    if (n>1) ret -= ret/n;
+    if (n > 1) ret -= ret / n;
     return ret;
 }
