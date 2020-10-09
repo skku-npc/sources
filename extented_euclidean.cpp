10c98a6212d932e8243e4f537a0933a7534e8667
Jaemin Choi <1dotolee@gmail.com>
2020-10-09 15:50:43 +0900

    Use tie in extended euclidean

--- extented_euclidean.cpp
+++ extented_euclidean.cpp
@@ -1,12 +1,10 @@
 pair<int, int> extended_euclidean(int a, int b) {
-    int s0 = 1, s1 = 0, t0 = 0, t1 = 1, r0 = a, r1 = b, q, r, s, t;
+    int s = 1, ss = 0, t = 0, tt = 1, r0 = a, r1 = b, q;
     while (r1 > 0) {
         q = r0 / r1;
-        r = r0 - q * r1; r0 = r1; r1 = r;
-        s = s0 - q * s1; s0 = s1; s1 = s;
-        t = t0 - q * t1; t0 = t1; t1 = t;
+        tie(r0, r1) = make_tuple(r1, r0 - q * r1);
+        tie(s, ss) = make_tuple(ss, s - q * ss);
+        tie(t, tt) = make_tuple(tt, t - q * tt);
     }
-    return {s0, t0};
+    return {s, t};
 }
-
-
