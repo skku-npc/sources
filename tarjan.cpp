1e2f135b4465dae6893c040cd4be31b1772714ef
Jaemin Choi <1dotolee@gmail.com>
2020-10-10 11:35:08 +0900

    Format tarjan

--- tarjan.cpp
+++ tarjan.cpp
@@ -1,41 +1,38 @@
-constexpr int MAXN = 100'00;
+constexpr int MAXN = 10000;
 
 int n, m; // 0-based
 vector<int> graph[MAXN];
-///////////////////
 int dfsn[MAXN], scc[MAXN];
 stack<int> stk;
 vector<vector<int>> res;
 int ce;
-int dfs(int now)
-{
+int dfs(int now) {
     dfsn[now] = ce++;
     int ret = dfsn[now];
     stk.push(now);
-    for (int nxt: graph[now])
-    {
-        if (dfsn[nxt] == -1) ret = min(ret, dfs(nxt));
-        else if (scc[nxt] == -1) ret = min(ret, dfsn[nxt]);
+    for (int nxt : graph[now]) {
+        if (dfsn[nxt] == -1)
+            ret = min(ret, dfs(nxt));
+        else if (scc[nxt] == -1)
+            ret = min(ret, dfsn[nxt]);
     }
-    if (ret == dfsn[now])
-    {
+    if (ret == dfsn[now]) {
         res.push_back(vector<int>());
         int t;
-        do
-        {
-            t = stk.top(); stk.pop();
+        do {
+            t = stk.top();
+            stk.pop();
             scc[t] = res.size() - 1;
             res.back().push_back(t);
-        } while(t != now);
+        } while (t != now);
     }
     return ret;
 }
-void set_scc()
-{
+void set_scc() {
     memset(dfsn, -1, sizeof dfsn);
     memset(scc, -1, sizeof scc);
     res.clear();
     ce = 0;
-    for (int i=0;i<n;++i)
+    for (int i = 0; i < n; ++i)
         if (dfsn[i] == -1) dfs(i);
 }
